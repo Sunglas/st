35395f37e42eeb064f3020a8ff4c84bef9734130
Sunglas <dsciencekid@gmail.com>
2023-09-07 21:55:23 -0400

    I couldn't undo the ligatures chang

--- st/config.def.h
+++ st/config.def.h
@@ -108,17 +108,7 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-<<<<<<< HEAD
 
-  [0] = "#323437",
-  [1] = "#ff5454",
-  [2] = "#8cc85f",
-  [3] = "#e3c78a",
-  [4] = "#80a0ff",
-  [5] = "#d183e8",
-  [6] = "#79dac8",
-  [7] = "#a1aab8",
-=======
 	[0] = "#323437",
 	[1] = "#ff5454",
 	[2] = "#8cc85f",
@@ -137,35 +127,14 @@ static const char *colorname[] = {
 	[13] = "#ae81ff",
 	[14] = "#85dc85",
 	[15] = "#e2637f",
->>>>>>> no-ligatures
 
-  [8] = "#7c8f8f",
-  [9] = "#ff5189",
-  [10] = "#36c692",
-  [11] = "#bfbf97",
-  [12] = "#74b2ff",
-  [13] = "#ae81ff",
-  [14] = "#85dc85",
-  [15] = "#e2637f",
-
-<<<<<<< HEAD
  	[255] = 0,
-=======
 	/* more colors can be added after 255 to use with DefaultXX */
 	[256] = "#282a36", 
 	[257] = "#f8f8f2",
 	[258] = "#080808",
 	[259] = "#eeeeee",
 };
->>>>>>> no-ligatures
-
- 	/* more colors can be added after 255 to use with DefaultXX */
-
-  [256] = "#282a36",
-  [257] = "#f8f8f2",
-  [258] = "#080808",
-  [259]= "#eeeeee",
- };
 
 /*
  * Default colors (colorname index)
@@ -173,13 +142,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 259;
 unsigned int defaultbg = 258;
-<<<<<<< HEAD
- static unsigned int defaultcs = 256;
- static unsigned int defaultrcs = 257;
-=======
 unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
->>>>>>> no-ligatures
 
 /*
  * Default shape of cursor
